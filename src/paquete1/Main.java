c96cf2dcb8e1916642dcca510a91240199ac997e
BKLP22 <97519892+BKLP22@users.noreply.github.com>
2022-02-11 09:02:37 +0100

    v2:Primer System.out.print

--- src/paquete1/Main.java
+++ src/paquete1/Main.java
@@ -2,7 +2,8 @@ package paquete1;
 
 public class Main
 {
-    public static void main(String[] args) {
-
+    public static void main(String[] args)
+    {
+        System.out.println("Hola mundo");
     }
 }
